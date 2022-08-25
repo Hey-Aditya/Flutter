f0eab09ce1c7dbd7f9955c09050d8e62ba4928ac
~Aditya <adityaaa.exe@gmail.com>
2022-08-25 12:04:07 +0530

    Create EnCounter.dart

---
+++ EnCounter.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'EnCounter!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle headline2 = Theme.of(context).textTheme.headline2!;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Button pushed this many times ~',
+              style: GoogleFonts.caudex(textStyle: headline2, color: Colors.deepOrange),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              '$_counter',
+              style: GoogleFonts.graduate(
+                textStyle: headline2.copyWith(
+                  fontStyle: FontStyle.italic,
+                ),
+                color: Colors.deepOrange,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
