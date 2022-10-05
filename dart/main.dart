af68678dcbc04b35e0ae2a2396ef577f4bd0c68a
~Aditya <adityaaa.exe@gmail.com>
2022-10-06 02:40:41 +0530

    Add files via upload
    6th Oct 2022

---
+++ dart/main.dart
@@ -0,0 +1,326 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+void lists() {
+  var names = ["Foo", "Bar", "Baz"];
+  // final foo = names[2];
+  print(names.length);
+  names.add("Hatt");
+  print(names.length);
+}
+
+void sets() {
+  var name = {"foo", "bar", "baz"};
+  name.add('fooo');
+  name.add('bar');
+  name.add('baz');
+  print(name);
+}
+
+void maps() {
+  var person = {
+    'age': 20,
+    'name': 'Foo',
+  };
+
+  print(person);
+  person['name'] = 'G00000000';
+  person['lastName'] = 'Hattbeyyy';
+  print(person);
+}
+
+void test() {
+  String? name = null;
+  print(name);
+  name = 'Tweety';
+  print(name);
+
+  List<String?>? names = ['Foo', 'Bar', null];
+  // names = null;
+  print(names);
+}
+
+void test2() {
+  const String? firstName = null;
+  const String? middleName = 'bar';
+  const String? lastName = 'baz';
+
+  const firstNonNullValue = firstName ?? middleName ?? lastName;
+}
+
+void test3(String? firstName, String? middleName, String? lastName) {
+  String? name = firstName;
+  name ??= middleName;
+  name ??= lastName;
+  print(name);
+}
+
+void test4(String? names) {
+  //brute force
+  // int length = 0;
+  // if (names != null) {
+  //   length = names.length;
+  // }
+  // print(length);
+
+  //optimized
+  final length = names?.length ?? 0;
+  print(length);
+}
+
+enum PersonProperties { firstName, lastName, age }
+
+void test5() {
+  print(PersonProperties.firstName.name);
+}
+
+enum AnimalType { cat, dog, sheep }
+
+void test6(AnimalType animalType) {
+  // Normal if-else statements
+  // if (animalType == AnimalType.cat) {
+  //   print("Isse accha toh kutta hi le leta.");
+  // } else if (animalType == AnimalType.dog) {
+  //   print("Billi behen ki lodi hai!");
+  // }
+
+  // Switch Statements
+  switch (animalType) {
+    // return- Ends completely and break- Continues through rest of the function
+    case AnimalType.cat:
+      {
+        print("Tom");
+        return;
+      }
+    case AnimalType.dog:
+      {
+        print("Courage aka SherDil");
+        return; // example
+      }
+    case AnimalType.sheep:
+      {
+        print("Shaun");
+        break; // example
+      }
+  }
+  print("Functions ends here.");
+}
+
+class Person {
+  void run() {
+    print("Running");
+  }
+
+  void breath() {
+    print("Breathing...");
+  }
+}
+
+void test7() {
+  //instances
+  final person = Person();
+  person.run();
+  person.breath();
+  Person();
+}
+
+class Person2 {
+  final String name;
+
+  Person2(this.name);
+
+  void printName() {
+    print("I will now print the name of this person!");
+    print(name);
+  }
+}
+
+void test8() {
+  final foo = Person2("Tweety");
+  print(foo.name);
+  foo.printName();
+}
+
+class LivingThing {
+  void breathe() {
+    print("This living thing is breathing");
+  }
+
+  void move() {
+    print("This is moving");
+  }
+}
+
+class Cat extends LivingThing {}
+
+void test9() {
+  final meow = Cat();
+  meow.move();
+  meow.breathe();
+}
+
+// Abstract Classes (Learn Thoroughly about this, I didn't understand a thing!)
+
+class Dog {
+  final String name;
+  Dog(this.name);
+
+  factory Dog.courage() {
+    return Dog("Sheru");
+  }
+}
+
+void test10() {
+  // final courage = Dog("Sher Dil");
+  final courage = Dog.courage();
+  print(courage.name);
+}
+
+class Catt {
+  final String name;
+  Catt(this.name);
+
+  @override
+  bool operator ==(covariant Catt other) => other.name == name;
+
+  @override
+  int get hashCode => name.hashCode;
+}
+
+void test11() {
+  final cat1 = Catt('Tom');
+  final cat2 = Catt('Tom');
+
+  if (cat1 == cat2) {
+    print("They are equal");
+  } else {
+    print("They are not equal");
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // lists();
+    // sets();
+    // maps();
+    // test2();
+    // test3(null, null, null);
+    // test4('Tweety');
+    // test5();
+    // test6(AnimalType.sheep);
+    // test7();
+    // test8();
+    // test9();
+    // test10();
+    test11();
+
+    return MaterialApp(
+      title: 'Flutter (0)',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutterly'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
